cdd6210cf5b5f64f3d31e4149b4d6ee77389a359
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2024-01-01 12:38:42 -0700

    Create Sample.java

---
+++ Algorithms/src/tests/Sample.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Sample
+{
+
+	@Test
+	void test()
+	{
+		assertEquals(1,1);
+	}
+
+}
